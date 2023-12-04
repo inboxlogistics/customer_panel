a16459136f6a0c43ec3431d7c9c85e4f1cd18376
rawasrazak3 <rawasrazak3@gmail.com>
2023-12-04 13:33:52 +0530

    new order updates

--- customer_panel/www/order_list/new_order.py
+++ customer_panel/www/order_list/new_order.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
-import frappe
-from frappe import _
+import frappe  , uuid,json
+from frappe import _, msgprint
 # from stripe import Order
 from yaml import load
 
@@ -52,3 +52,66 @@ def get_item_code():
         ]
     )
     return item_code_list
+
+@frappe.whitelist(allow_guest=True)
+def get_customer():
+    logged_in_user = frappe.session.user
+    userpermission_list = frappe.get_all('User Permission',filters={'user':logged_in_user,'allow':'Customer'},fields=['for_value'])
+    if(len(userpermission_list)<=0):
+        frappe.throw(_("No Customer Record Found for your Login"), frappe.PermissionError)
+    customer_name = userpermission_list[0].for_value
+    return customer_name
+@frappe.whitelist(allow_guest=True)
+def save_order_delivery(end_user_name,
+                        end_user_phone_number,
+                        delivery_date_and_preferred_timing,
+                        location,
+                        building_no,
+                        zone_no,
+                        street,
+                        notes,
+                        arr
+                        ):
+    customer_name = get_customer()
+    doc = frappe.get_doc({'doctype':'Delivery Dashboard Form'})
+    
+    doc.client_name= str(customer_name)
+    doc.order_status="New"
+    doc.end_user_name=str(end_user_name)
+    doc.end_user_phone_number=str(end_user_phone_number)
+    doc.delivery_date_and_preferred_timing=str(delivery_date_and_preferred_timing)
+    # doc.delivery_time_range=str(delivery_time_range)
+    doc.location=str(location)
+    doc.building_no=str(building_no)
+    doc.zone_no=str(zone_no)
+    doc.street=str(street)
+    doc.notes=str(notes)
+    doc.total_discounts=0
+    data =json.loads(arr)
+    for i in data:
+        # return int(i[2])
+        if len(i)==0:
+            sumqty=0
+            sumvolume=0
+            totalprice=0
+        else:
+            doc.append("ecommerce_item",{"item_code":str(i[5]),"volume":str(i[8]),"uom":str(i[4]),"qty":str(i[2]),"amount":str(i[3]),"description":i[1]})
+            arrqty=[]
+            arrqty.append(int(i[2]))
+            sumqty=sum(arrqty)
+            arrvolume=[]
+            arrvolume.append(float(i[7]))
+            sumvolume=sum(arrvolume)
+            arrprice=[]
+            arrprice.append(float(i[6]))
+            totalprice=sum(arrprice)
+    doc.total_volume= sumvolume
+    doc.total_qty=sumqty
+    doc.collection_amount=totalprice
+    doc.save()
+    return "SaveOrder"
+
+@frappe.whitelist(allow_guest=True)
+def selectItemCodeData(item_code):
+    alldata=frappe.db.sql("""SELECT `tabItem`.item_name,`tabItem`.stock_uom ,`tabItem Barcode`.barcode, `tabItem`.volume_cf FROM tabItem INNER JOIN `tabItem Barcode` ON `tabItem Barcode`.parent =`tabItem`.name  WHERE  `tabItem`.name =%s """,item_code)
+    return alldata
